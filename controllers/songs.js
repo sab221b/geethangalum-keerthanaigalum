21554f7beff7a00d903d3c01c6a3fe3591ec391c
sab221b <sab221b@gmail.com>
2023-10-02 03:14:32 +0530

    add img to text ocr

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,6 +1,8 @@
 var path = require('path');
 const songsIndex = path.resolve('assets/source/sortlist.txt')
 const fs = require('fs');
+const { createWorker } = require('tesseract.js');
+
 
 exports.listSongs = (req, res) => {
     fs.readFile(songsIndex, 'utf8', (err, data) => {
@@ -28,12 +30,33 @@ exports.listSongs = (req, res) => {
 exports.getSongbyNumber = (req, res) => {
     console.log('req.params', req.params.songNo);
     if (!isNaN(req.params.songNo)) {
-        var songSourceNo = (Number(req.params.songNo) - 1).toString();
-        const selectedSong = path.resolve('assets/source/' + 'd' + songSourceNo + '.txt');
+        var songSourceNo = (Number(req.params.songNo)).toString();
+        const selectedSong = path.resolve('assets/new-source/' + songSourceNo + '.txt');
         fs.readFile(selectedSong, 'utf8', (err, data) => {
             if (err) throw err;
             var songText = data.split('\n');
             res.render('selected-song', { title: 'கீதங்களும் கீர்த்தனைகளும்', songLyrics: songText });
         });
     }
+}
+
+exports.ImagetoTextOCR = async (req, res) => {
+    const worker = await createWorker('tam');
+    const folderPath = 'assets/image-canvases';
+    fs.readdir(folderPath, async (error, files) => {
+        if (error) {
+            console.log(error);
+        } else {
+            for (let i = 0; i < files.length; i++) {
+                if (i === files.length) {
+                    await worker.terminate();
+                    break;
+                }
+                const fileName = files[i]
+                const canvasImg = path.resolve(`assets/image-canvases/${fileName}`)
+                const { data: { text } } = await worker.recognize(canvasImg);
+                fs.writeFileSync(`assets/new-source/${fileName.replace(".png", ".txt")}`, text);
+            }
+        }
+    });
 }
--- routes/pages_routes.js
+++ routes/pages_routes.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const songsCtrl = require("../controllers/songs");
 
-router.get("/", songsCtrl.listSongs);
+router.get('/', songsCtrl.listSongs);
 router.get('/:songNo', songsCtrl.getSongbyNumber);
 
 module.exports = router;
+        
